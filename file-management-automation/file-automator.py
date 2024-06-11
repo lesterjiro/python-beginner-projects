34efd5ce0180d9e700bbae88c78ca4fa39d0038d
lesterjiro <lesterjeromeortiz@gmail.com>
2024-06-11 21:31:33 +0800

    rewrote some lines

--- file-management-automation/file-automator.py
+++ file-management-automation/file-automator.py
@@ -1,36 +1,36 @@
+import shutil 
 import os
-import shutil
 
-def organize_files_by_extension(source_dir):
+def file_organizer(source_dir):
     try:
         files = os.listdir(source_dir)
     except FileNotFoundError:
-        print(f"Oops... The path {source_dir} doesn't exist.")
+        print(f"The path {source_dir} doesn't exist.")
 
     for file in files:
         file_path = os.path.join(source_dir, file)
-        
-        # ? Skip directories 
+
+        # ? Skip directories
         if os.path.isdir(file_path):
             continue
-        
+
         filename, extention = os.path.splitext(file)
         extention = extention[1:].upper() + 's'
 
-        # ? Skip files without extensions
+        # ? Skip file without extentions
         if not extention:
             continue
-        
+
         destination_dir = os.path.join(source_dir, extention)
 
         # ? Make directory if it doesn't exist
         if not os.path.exists(destination_dir):
             os.makedirs(destination_dir)
 
-        # ? Move the file
+        # ? Move file
         shutil.move(file_path, os.path.join(destination_dir, file))
 
-    print("All files are organized.")
-          
+    print("Files are now organized.")
+
 source_dir = r"C:\Users\ortiz\Downloads"
-organize_files_by_extension(source_dir)
+file_organizer(source_dir)
